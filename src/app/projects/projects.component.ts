830995f133d15cdd15370dfc2f37fbb2b65cc66f
neville.walo@gmail.com <neville.walo@gmail.com>
2019-02-13 14:42:25 +0100

    updtaed nutton

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,6 +3,7 @@ import {Project} from './Project';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
@@ -11,33 +12,48 @@ import {map} from 'rxjs/operators';
 })
 export class ProjectsComponent implements OnInit {
   private Projects: Array<Project>;
+  name = new FormControl('', [Validators.required]);
+  Message: FormGroup;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver, private fb: FormBuilder) {
+    this.Message = fb.group({
+      name: this.name,
+    });
   }
 
   ngOnInit() {
     this.Projects = this.getAllProjects();
   }
 
-//Returns all projects in an array!
+// Returns all projects in an array!
   private getAllProjects(): Array<Project> {
     return undefined;
   }
 
-  //Adds a new Project to the Blockchain
+  // Adds a new Project to the Blockchain
   add(projectname: string): boolean {
     return true;
   }
 
-  //Vote woih amout of token on a given project
+  // Vote woih amout of token on a given project
   voteOn(projectname: string, amount: number): boolean {
-    return true; //AY, DY
+    return true; // AY, DY
   }
 
 
+  commit() {
+    if (this.Message.valid) {
+      if (this.add(this.name.value)) {
+        alert('You added' + this.name.value + 'to the community projects');
+      }
+
+
+    }
+
+  }
 }
