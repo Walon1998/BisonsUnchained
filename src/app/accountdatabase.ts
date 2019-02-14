d3f43d1728cb129a31afc02b3a443023cadf6eea
WaloNeville <neville.walo@gmail.com>
2019-02-14 22:57:00 +0100

    login ready

--- src/app/accountdatabase.ts
+++ src/app/accountdatabase.ts
@@ -1,6 +1,6 @@
 import {Account} from './account';
 
 export const AccountDatabase: Array<Account> = [
-  new Account('hunter', 'hunter', '0x98141980234710293847', '0xhjbakdfhbadkfhb'),
-  new Account('hunter2', 'hunter2', '0x98141980234710293847', '0xhjbakdfhbadkfhb'),
+  new Account('hunter', 'hunter', '0x123...', '0x123...'),
+  new Account('hunter2', 'hunter2', '0x123...', '0x123...'),
 ];
